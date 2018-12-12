e105f2e8a71fb264a587d8690e1edc0fe3d044d3
anders <andersge92@gmail.com>
2018-12-12 10:00:54 +0100

    prettied up a bit

--- source/main.cpp
+++ source/main.cpp
@@ -9,14 +9,16 @@ int main()
     int product1 = 4;
     int product2 = 5;
     Base *base_ptr;
-    Derived derived_obj( a, b);
-    base_ptr = &derived_obj;
-    //base_ptr = new Derived( int 2, int 3 );
+
+    base_ptr = new Derived( a, b );
 
     base_ptr -> setParam( product1, product2);
-    base_ptr -> calculate();
 
-    std::cout << "The product of " << product1 << " and " << product2 << " is " << base_ptr ->calculate() << std::endl;
+    std::cout
+        << "The product of " << product1
+        << " and " << product2
+        << " is " << base_ptr ->calculate()
+        << std::endl;
 
     return 0;
 }; // end main
