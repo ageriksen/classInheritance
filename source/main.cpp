cb49576bcb98ebaf17de8b7fb09389cc5906121b
anders <andersge92@gmail.com>
2018-12-12 09:58:16 +0100

    it now compiles and runs properly

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,22 @@
 #include "base.h"
 #include "derived.h"
+#include <iostream>
 
 int main()
 {
+    int a = 2;
+    int b = 3;
+    int product1 = 4;
+    int product2 = 5;
     Base *base_ptr;
-    Derived derived_obj( int 2, int 3);
+    Derived derived_obj( a, b);
     base_ptr = &derived_obj;
     //base_ptr = new Derived( int 2, int 3 );
 
-    base_ptr -> setParam( int 1, int 5);
+    base_ptr -> setParam( product1, product2);
     base_ptr -> calculate();
+
+    std::cout << "The product of " << product1 << " and " << product2 << " is " << base_ptr ->calculate() << std::endl;
 
     return 0;
 }; // end main
